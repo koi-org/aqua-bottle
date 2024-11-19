a3f7c1bf93c66db0ba18f4e6c398035516b07ae2
Andy Ruan <z5590336@ad.unsw.edu.au>
2024-11-19 15:15:15 +1100

    initial bot setup

---
+++ main.py
@@ -0,0 +1,56 @@
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+
+# load our token somewhere safe
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+# bot setup
+intents: Intents = Intents.default()
+intents.message_content = True  # NOQA
+client: Client = Client(intents=intents)
+
+# STEP 2: MESSAGE FUNCTIONALITY
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(Message was empty because intents were not enabled probably)')
+        return
+    
+    is_private = user_message[0] == '?'
+    
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+# STEP 3: HANDLING THE STARTUP FOR OUR BOT
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+# STEP 4: HANDLING INCOMING MESSAGE
+@client.event
+async def on_message(message: Message) -> None:
+    if message.auther == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message : str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+# STEP 5: MAIN ENTRY POINT
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__'"
+    main()
