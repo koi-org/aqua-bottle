51e289067f74b09a100b1f92588a3b33765e59d7
ultifinity <andiep.sf@gmail.com>
2024-12-25 23:32:03 -0800

    Add data type check

--- src/data.py
+++ src/data.py
@@ -2,6 +2,11 @@ import json
 import os
 from filelock import FileLock
 
+def ensure_dict(value):
+    if not isinstance(value, dict):
+        raise TypeError("not a dict :(")
+    return value
+
 def read_data(user_id):
     user_file = f"./data/{user_id}.json"
     lock_file = f"{user_file}.lock"
@@ -12,6 +17,7 @@ def read_data(user_id):
             with open(user_file, 'r') as f:
                 user_data = json.load(f)
 
+            ensure_dict(user_data)
             return user_data
         except FileNotFoundError:
             print(f"file not found: {user_file}")
