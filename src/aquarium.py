a3d467eec0d4a0050353ae969fa796530ac39d16
Andy <andy.ruan2005@gmail.com>
2024-12-29 13:55:16 +1100

    linted aquarium.py code

--- src/aquarium.py
+++ src/aquarium.py
@@ -2,8 +2,11 @@
 This module defines the Aquarium class, which simulates an aquarium's features 
 and provides methods to manage its environment.
 """
+
+
 class Aquarium:
     """The aquarium class"""
+
     def __init__(
         self,
         channel_id: int,
@@ -20,12 +23,9 @@ class Aquarium:
             "nitrate": 0,
             "nitrite": 0,
             "ph": None,
-            "temperature": None
-            }
-        self.inhabitants = {
-            'fish': {},
-            'plants': {}
+            "temperature": None,
         }
+        self.inhabitants = {"fish": {}, "plants": {}}
 
     def choose_substrate(self, substrate: str):
         """Method to choose the aquarium's substrate"""
