618c537084aaf22651490ef42c86346bc2cde04f
ultifinity <andiep.sf@gmail.com>
2024-12-29 23:40:59 -0800

    Refactor non class manager to class manager

--- src/manager.py
+++ src/manager.py
@@ -1,28 +1,32 @@
 from user import User
 from typing import Dict
 
-# Manage all users here
-manager: Dict[int, User] = {}
+class Manager:
+    # Manage all users here
+    users: Dict[int, User] = {}
 
-def add_user(user_id: int, username: str) -> bool:
-    if user_id in manager:
-        return False
+    @staticmethod
+    def add_user(user_id: int, username: str) -> bool:
+        if user_id in Manager.users:
+            return False
 
-    new_user = User(username, user_id, 100)
-    manager[user_id] = new_user
-    return True
+        new_user = User(username, user_id, 100)
+        Manager.users[user_id] = new_user
+        return True
 
-def get_user(user_id: int):
-    user = manager.get(user_id)
-    if user:
-        return user
-    else:
-        print(f"User {user_id} not found.")
-        return None
+    @staticmethod
+    def get_user(user_id: int):
+        user = Manager.users.get(user_id)
+        if user:
+            return user
+        else:
+            print(f"User {user_id} not found.")
+            return None
 
-def list_aquariums(user_id: int):
-    user = manager.get(user_id)
-    if user:
-        print(vars(user.aquarium))
-    else:
-        print(f"User {user_id} not found.")
+    @staticmethod
+    def list_aquariums(user_id: int):
+        user = Manager.users.get(user_id)
+        if user:
+            print(vars(user.aquarium))
+        else:
+            print(f"User {user_id} not found.")
