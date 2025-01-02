7d1b2fbe09cb989d23333769707e3aa773985f8a
ultifinity <andiep.sf@gmail.com>
2025-01-01 17:03:29 -0800

    Make globals into static attributes

--- src/classes/aquarium.py
+++ src/classes/aquarium.py
@@ -5,14 +5,10 @@ This module defines the Aquarium class, which simulates an aquarium's features
 and provides methods to manage its environment.
 """
 
-valid_substrate = {"gravel", "sand", "soil"}
-
-
-valid_fish = {"guppy", "neon_tetra", "molly", "platy"}
-
-
 class Aquarium:
     """The aquarium class"""
+    valid_fish = {"guppy", "neon_tetra", "molly", "platy"}
+    valid_substrate = {"gravel", "sand", "soil"}
 
     def __init__(
         self,
@@ -41,11 +37,6 @@ class Aquarium:
     def add_heater(self):
         """Method to add a heater to the fish tank"""
         self.heater = True
-
-    def is_valid_fish(self, fish: Fish):
-        if fish.species not in valid_fish:
-            return False
-        return True
 
     def add_fish(self, fish: Fish):
         self.inhabitants["fish"].add(fish)
