c61f0e6cb0d8ba81578e62fa4ef851f6dfd9f4ce
Andy <andy.ruan2005@gmail.com>
2025-01-03 14:10:52 +1100

    written documentation to fish class

--- src/classes/fish.py
+++ src/classes/fish.py
@@ -1,10 +1,23 @@
-"""
-    This class contains the information and stats of a fish
-"""
-
-
 class Fish:
-    def __init__(self, species, gender, age_months):
+    """
+    A class to represent fish
+    """
+    def __init__(self, species: str, gender: str, age_months: int):
+        """
+        Initialises fish instance with species gender and age in months
+
+        Attributes:
+            species: str
+                the species of the fish
+            gender: string
+                gender of the fish
+            age: int
+                age of the person
+
+        Methods
+        ------
+        TODO: add methods later
+        """
         self.species = species
         self.gender = gender
         self.age = age_months
