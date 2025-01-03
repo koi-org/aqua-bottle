6ec216af3f531de6701f3cfb35cc4024cb077b1e
Andy <andy.ruan2005@gmail.com>
2025-01-03 15:17:40 +1100

    created docstrings for miscellaneous commands

--- src/cogs/miscellaneous_cmds.py
+++ src/cogs/miscellaneous_cmds.py
@@ -3,13 +3,31 @@ from discord.ext import commands
 
 
 class Miscellaneous(commands.Cog):
+    """
+    A cog command for miscellaneous commands
+    """
     def __init__(self, bot):
+        """
+        Initialises the Miscellaneous Cog.
+
+        Parameters:
+            bot (commands.Bot): The Discord bot instance to which this Cog is added.
+        """
         self.bot = bot
 
     @discord.slash_command(
         name="hello", description="Say hello to the bot", guild_ids=[692964332643942463]
     )
     async def hello(self, ctx: discord.ApplicationContext):
+        """
+        Slash command to say hello to the user
+
+        Parameters:
+            ctx (discord.ApplicationContext): The context of the command.
+
+        Responds by saying hello to the user with their username and user id and
+        channel id
+        """
         username = ctx.author.name
         user_id = ctx.author.id
         channel_id = ctx.channel.id
