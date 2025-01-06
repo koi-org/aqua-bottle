2a657a1d60c2736d0d8f8f193f7a38f792250efe
ultifinity <andiep.sf@gmail.com>
2025-01-05 17:29:14 -0800

    Simplify aquarium

--- src/classes/aquarium.py
+++ src/classes/aquarium.py
@@ -3,71 +3,17 @@ import datetime
 import threading
 import time
 
-
-valid_fish = {"Guppy", "Neon Tetra", "Molly", "Platy"}
-valid_substrate = {"Gravel", "Sand", "Soil"}
-
-
 class Aquarium:
-    """
-    A class to represent an aquarium.
-
-    ...
-    Attributes
-    ----------
-    channel_id : int
-        the channel id of where the aquarium is located
-    cycled: bool
-        checks if the aquarium is ammonia cycled
-    volume : int
-        the volume of the aquarium in litres
-    substrate : str
-        the substrate of the aquarium
-    heater : bool
-        checks whether the tank has a heater
-    water_quality : dict
-        the water quality of a tank, such as pollution, pH and temperature
-
-    Methods
-    --------
-        choose_substrate(substrate):
-            chooses the substrate for the aquarium
-        add_heater():
-            adds a heater to the aquarium
-        add_fish(fish: Fish):
-            adds fish to the aquarium
-    """
-
+    valid_substrate = {"Gravel", "Sand", "Soil"}
     time_unit = 5
 
-    def __init__(
-        self,
-        channel_id: int,
-        volume: int,
-    ):
-        """
-        Construcuts necessary attributes for the aquarium object.
-
-        Parameters
-        ---------
-            channel_id : int
-                the channel id of where the aquarium is located
-            volume : int
-                the volume of the aquarium in litres
-        """
-
+    def __init__(self, channel_id: int, volume: int, substrate: str):
         self.channel_id = channel_id
         self.cycled = False
         self.volume = volume
-        self.substrate = None
+        self.substrate = substrate
         self.heater = False
-        self.water_quality = {
-            "ammonia": 0,
-            "nitrate": 0,
-            "nitrite": 0,
-            "ph": None,
-            "temperature": None,
-        }
+        self.water_quality = 0
         self.inhabitants = {"fish": set(), "plants": set()}
 
         # timer
